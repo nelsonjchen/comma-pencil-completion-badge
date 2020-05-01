eea4b2a180bc94acca369104bfd2df0f708450fb
Nelson Chen <crazysim@gmail.com>
2020-04-30 18:10:16 -0700

    Fix mask count

--- generate-badge.py
+++ generate-badge.py
@@ -13,9 +13,9 @@ touched_masks_count_and_pairs = [
     # Hard 100
     TouchedMasks(100, "6bfbd3202cc88071d9b48b41cd75ecb63f1bf666~1 0c2e5ee5e4f2f72ab0c2e2521344b1035fdaddba"),
     # Requests 2
-    TouchedMasks(100, "43e5683571a41e0e8713f69ceb9c18e0feeb9678~1 6bfbd3202cc88071d9b48b41cd75ecb63f1bf666"),
+    TouchedMasks(2, "43e5683571a41e0e8713f69ceb9c18e0feeb9678~1 6bfbd3202cc88071d9b48b41cd75ecb63f1bf666"),
     # Requests 5
-    TouchedMasks(100, "HEAD 43e5683571a41e0e8713f69ceb9c18e0feeb9678"),
+    TouchedMasks(6, "HEAD 43e5683571a41e0e8713f69ceb9c18e0feeb9678"),
 ]
 
 mask_count = sum(t.count for t in touched_masks_count_and_pairs)
